afca2612aeb93921860fabe54417e877711ad2fe
Lyokan <Lyokanprogramming@protonmail.com>
2021-07-04 23:34:07 +1000

    Code cleanup, QoL improvements

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,51 +1,102 @@
 VALID_CHOICES = %w(rock paper scissors spock lizard)
+WIN_CONDITIONS = { 'lizard' => %w[spock paper],
+                   'paper' => %w[rock spock],
+                   'scissors' => %w[paper lizard],
+                   'rock' => %w[scissors lizard],
+                   'spock' => %w[scissors rock] }
 
 def prompt(message)
   puts("=> #{message}")
 end
 
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'spock' && second == 'rock') ||
-    (first == 'spock' && second == 'scissors') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'lizard' && second == 'paper')
-end
-
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You win!")
-  elsif win?(computer, player)
-    prompt("You lose!")
-  else
-    prompt("it was a tie!")
+def win?(player, computer)
+  if WIN_CONDITIONS[player].include?(computer)
+    true
   end
 end
 
-loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(' ')}.")
-    choice = gets.chomp.downcase
+def display_round_results(player, computer)
+  if win?(player, computer)
+    prompt("You win the round!")
+  elsif win?(computer, player)
+    prompt("You lose the round!")
+  else
+    prompt("The round was a tie!")
+  end
+end
 
-    if VALID_CHOICES.include?(choice)
+def choice_shortcut(choice)
+  case choice
+  when "r" then "rock"
+  when "p" then "paper"
+  when "sc" then "scissors"
+  when "s" then "spock"
+  when "l" then "lizard"
+  end
+end
+
+def increment_score(first, second)
+  if win?(first, second)
+    1
+  else
+    0
+  end
+end
+
+def display_winner(player_score, computer_score)
+  if player_score == 5
+    prompt("You win! Your score was #{player_score} to #{computer_score}")
+  elsif computer_score == 5
+    prompt("You lost! Your score was #{player_score} to #{computer_score}")
+  end
+end
+
+def display_scores(player_score, computer_score)
+  prompt("Your score is #{player_score} to #{computer_score}")
+end
+
+loop do # Program loop
+  choice = nil
+
+  player_score = 0
+  computer_score = 0
+
+  loop do # Game loop
+    loop do # player input loop
+      prompt("Choose one: (r)ock, (p)aper, (sc)issors, (s)pock, (l)izard")
+      choice = gets.chomp.downcase
+
+      if choice.length == 1
+        choice = choice_shortcut(choice)
+      end
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{choice}; Computer chose: #{computer_choice}")
+
+    player_score += increment_score(choice, computer_choice)
+    computer_score += increment_score(computer_choice, choice)
+
+    display_round_results(choice, computer_choice)
+
+    if player_score < 5 && computer_score < 5
+      display_scores(player_score, computer_score)
+    end
+
+    display_winner(player_score, computer_score)
+
+    if player_score == 5 || computer_score == 5
       break
-    else
-      prompt("That's not a valid choice")
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
+  print("\n")
   prompt("Do you want to play again?")
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
